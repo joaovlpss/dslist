6e8edb95c2b15e14c7b50e649018fafa404dffd1
Joao Victor Lopes <joaovlpssdev@gmail.com>
2023-11-14 21:32:54 -0300

    Implemented GameDTO, findById.

--- src/main/java/com/joaovlpss/dslist/services/GameService.java
+++ src/main/java/com/joaovlpss/dslist/services/GameService.java
@@ -5,11 +5,14 @@
 package com.joaovlpss.dslist.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.joaovlpss.dslist.repositories.GameRepository;
+import com.joaovlpss.dslist.dto.GameDTO;
 import com.joaovlpss.dslist.dto.GameMinDTO;
 import com.joaovlpss.dslist.entities.Game;
 
@@ -22,10 +25,22 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
     // Spring automatically does the queries and returns the results
     List<Game> result = gameRepository.findAll();
     List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
     return dto;
     }
-}
+
+    
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Optional<Game> optionalGame = gameRepository.findById(id);            
+        if (optionalGame.isPresent()) {
+            Game entity = optionalGame.get();
+            return new GameDTO(entity);
+        }
+            return null;
+        }
+    }
